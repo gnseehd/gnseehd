51770d9ebe6879ac7a33af5e936e460619f8d65e
gnseehd <95120135+gnseehd@users.noreply.github.com>
2021-12-05 18:30:36 +0900

    Add files via upload

---
+++ note_1.py
@@ -0,0 +1,46 @@
+# 031
+# a = '3'
+# b = '4'
+# print(a+b)
+
+# 032
+# print('hi'*3)
+
+# 033
+# print('-'* 80)
+
+# 034
+# t1 = 'python'
+# t2 = 'java'
+# print((t1 + t2)*4)
+# str, str = 'str', 'str'  str+str = str str
+
+# 035
+# name1 = '김민수'
+# age1 = 10
+# name2 = '이철희'
+# age2 = 14
+# print(f'이름: {name1} 나이: {age1}\n이름: {name2} 나이: {age2}  ')
+
+# 036, 037 pass 
+
+# 038 ,제거 이후 정수형으로 변환
+# samsung = '5,969,782,550'
+# samsung = samsung.replace(',','')
+# samsung = int(samsung)
+# print(samsung)
+# print(type(samsung))
+
+# 039
+# qua = '2020/03(E) (IFRS연결)'
+# qua = qua[0:7]
+# print(qua)
+
+# 040 strip 함수
+# data = '   삼성전자   '
+# print(data.strip())
+# strip = 맨 앞, 맨 뒤 writespace를 제거해준다.
+# wirtespace = ' ', '\t', '\n' 등을 포괄적으로 의미함.
+
+import os
+os.getcwd()
