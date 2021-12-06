a666777d0488bbe87500ef156ce5faae581250c8
gnseehd <95120135+gnseehd@users.noreply.github.com>
2021-12-06 15:23:36 +0900

    Add files via upload

---
+++ list_study.py
@@ -0,0 +1,93 @@
+# 051
+# movie_rank = ['닥터스트레인지', '스플릿', '럭키']
+# print(movie_rank)
+
+# 052
+# movie_1 = '배트맨'
+# movie_rank.append(movie_1)
+# print(movie_rank)
+
+# 053
+# movie_2 = '슈퍼맨'
+# movie_rank.insert(1,movie_2) # list.insert(지정 index,요소) - 리스트 내 요소 삽입 함수
+# print(movie_rank)
+
+# 054
+# movie_rank.remove('럭키')
+# del movie_rank[3] # del list[index] - 리스트 내 해당 인덱스 위치의 요소 제거
+# print(movie_rank)
+
+# 055
+# del movie_rank[2:]
+# print(movie_rank)
+
+# 056
+# lang1 = ["C", "C++", "JAVA"]
+# lang2 = ["Python", "Go", "C#"]
+# lang = lang1 + lang2
+# print(lang)
+
+# 057
+# nums = [1, 2, 3, 4, 5, 6, 7]
+# max = max(nums)
+# min = min(nums)
+# print(f'max = {max}\nmin = {min}')
+
+# 058 
+# nums = [1, 2, 3, 4, 5]
+# total = 0
+# for i in nums:
+#     total += i
+# print(total)
+
+# 059
+# cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"]
+# # print(len(cook))
+
+# 060
+# nums = [1, 2, 3, 4, 5]
+# sum_nums = sum(nums)
+# print(sum_nums/len(nums))
+
+# 061
+# price = ['20180728', 100, 130, 140, 150, 160, 170]
+# print(price[1:]) 
+
+# 062 
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(nums[0::2])
+
+# 063
+# print(nums[1::2])
+
+# 064
+# nums = [1, 2, 3, 4, 5]
+# print(nums[-1::-1])
+
+# 065 
+# interest = ['삼성전자', 'LG전자', 'Naver']
+# print(interest[0] ,interest[2])
+
+# 066
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# for name in interest:
+#     print(name,end=' ')
+
+# 067
+# for name in interest:
+#     print(name,end='/')
+
+# 068
+# for name in interest:
+#     print(name)   
+
+# 069
+# string = "삼성전자/LG전자/Naver"
+# interest = string.split('/')
+# print(interest)
+
+# 070
+# data = [2, 4, 3, 1, 5, 10, 9]
+# data.sort(reverse=1)
+# print(data)
+
